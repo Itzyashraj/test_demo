318c67701a11cdb21c4557de99abb4d66477e8ff
Yashraj Anil Mhase <145848542+Itzyashraj@users.noreply.github.com>
2025-03-23 19:43:11 +0530

    Add files via upload

---
+++ numpy.py
@@ -0,0 +1,14 @@
+#called so because of numerical python , 2005 by travis oliphant 
+#numpy is package for multi-D data , math functions , linear algebra , all matrix operations and their rules , arrays 
+# arrays are used here -- they store elements in continous memory locations  , store item of fix size / name and same datatype , easy for maths operation
+
+import numpy as np
+
+# l1 = [1,2,3]
+# l2 = [4,5,6]
+# print(l1+l2)  this gives the concatenation not the sum
+
+file1 = np.array([10,20,30,40,50])
+file2 = np.array([60,70,80,90,100])
+print("my file1 is \n",file1)
+print("my file2 is \n",file2)
